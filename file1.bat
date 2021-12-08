8b1c9ada6e9509985d9b9aa59715ef93af34d9a4
Avaneesh <84514598+Avaneesh-alake@users.noreply.github.com>
2021-12-08 17:42:30 +0530

    Add files via upload

---
+++ file1.bat
@@ -0,0 +1,10 @@
+set variable=HELLO WORLD
+pause
+echo %variable%
+pause
+echo now we are concatinating
+set variable=%variable% By Avaneesh
+echo %variable%
+pause
+echo 12345
+pause
---
+++ file2.bat
@@ -0,0 +1,13 @@
+dir REQT practice
+vol
+pause
+path
+pause
+cls
+vol C:
+pause
+cls
+vol E:
+pause
+echo spacebar to exit
+exit
